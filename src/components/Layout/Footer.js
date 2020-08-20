f1d38e5401db39295f4634e46dfc2c188b3c1843
Arman Sidhu <61654102+armansidhu3@users.noreply.github.com>
2020-08-21 01:09:08 +0530

    First commit.

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Footer = () => (
+  <div className="footer">
+    <p>Designed and developed by: Arman<a rel="noopener noreferrer" target="_blank" href="https://github.com/armansidhu3"><br/>My GitHub</a></p>
+  </div>
+)
+
+export default Footer
